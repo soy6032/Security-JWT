a8d71bfabbe4046f063cf0237f4cd0c468f8b614
soy6032 <404444@naver.com>
2023-11-03 15:44:15 +0900

    note delete

--- src/main/java/com/project/jpa1/controller/UserController.java
+++ src/main/java/com/project/jpa1/controller/UserController.java
@@ -2,25 +2,15 @@ package com.project.jpa1.controller;
 
 import com.project.jpa1.config.auth.LoginUser;
 import com.project.jpa1.dto.*;
-import com.project.jpa1.entity.Note;
-import com.project.jpa1.entity.User;
-import com.project.jpa1.repository.UserRepository;
-import com.project.jpa1.service.NoteService;
 import com.project.jpa1.service.UserService;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import  com.project.jpa1.dto.UserReqDto.JoinReqDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -28,7 +18,6 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
-    private final NoteService noteService;
 
     @PostMapping("/join")
     public ResponseEntity<?> join(@RequestBody @Valid UserReqDto.JoinReqDto joinReqDto, BindingResult bindingResult) {
